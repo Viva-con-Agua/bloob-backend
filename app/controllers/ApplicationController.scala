38a42c13842ce70dfbb2caa15e0eaa02c68c0545
Tom Bierschenk <bierscht@hu-berlin.de>
2019-07-09 14:06:55 +0200

    refactoring

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,31 +1,23 @@
 package controllers
 
 import javax.inject._
-import java.util.UUID
-import play.api.libs.json._
-import play.api.libs.functional.syntax._
-import play.api.libs.ws.WSClient
-import play.libs.ws._
-import utils.{Ascending, Descending, SortDir}
-import play.api.Configuration
-import models.{EmailAccessRight, EmailARRequest, EmailARDeleteRequest, Email}
+
 import play.api.Logging
 import play.api.mvc._
 import play.api.libs.json.{JsError, JsValue, Json}
-import services.{EmailAccessRightsService, EmailService, MailerService}
+import play.api.Configuration
 
+import java.util.UUID
+
+import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-import scala.concurrent.Await
-import scala.concurrent.duration._
 
 import com.mohiva.play.silhouette.api.Silhouette
 import org.vivaconagua.play2OauthClient.silhouette.{CookieEnv, UserService}
 import org.vivaconagua.play2OauthClient.drops.authorization._
 
-
-import scala.concurrent.{ExecutionContext, Future}
-
+import models.{EmailAccessRight, EmailARRequest, EmailARDeleteRequest, Email}
+import services.{EmailAccessRightsService, EmailService, MailerService}
 
 @Singleton
 class ApplicationController @Inject()(
@@ -35,7 +27,6 @@ class ApplicationController @Inject()(
   emailARService: EmailAccessRightsService,
   emailService: EmailService,
   mailerService: MailerService,
-  ws: WSClient,
   config: Configuration,
 ) extends AbstractController(cc) with Logging {
 
@@ -132,160 +123,18 @@ class ApplicationController @Inject()(
     }
   }
   def testDropsRest() = Action.async { implicit request =>
-    println("hello test")
     
     //var aUuid = UUID.fromString("c3702bf6-9e98-4b7b-957e-261ea12c552c")
-    var aUuid = UUID.fromString("58fd2d56-fa65-4a11-a3a8-7991cadc1809")
+    //var aUuid = UUID.fromString("58fd2d56-fa65-4a11-a3a8-7991cadc1809")
     //var aUuid = UUID.fromString("c6621c0c-7e6b-4706-a790-8e8a49b7a603")
     
-    println(aUuid)
     var listUuid = List(UUID.fromString("c3702bf6-9e98-4b7b-957e-261ea12c552c"),UUID.fromString("58fd2d56-fa65-4a11-a3a8-7991cadc1809"))
-    var requestC = UserCrewRequest(listUuid)
-    var requestString = requestC.toString
-    println(path)
-    println(requestString)
-    var myresult=Await.result(
-    ws.url(path)
-      .addHttpHeaders("Content-Type" -> "application/json")
-      .addQueryStringParameters("client_id" -> client_id, "client_secret" -> client_secret)
-      .post(requestString)
-    ,10 seconds
-    ).body
-
-    var myJsonResult = Json.parse(myresult)
-    //var myJsonResultArray = JSArray
-    //myJsonResult.map (x => println(x))
-    println(myJsonResult \\ "profiles")
-
-    var requestedEmails = (myJsonResult \\ "profiles").map ( x => 
-      ( (x(0) \ "email" ) ).get
-    )
-
-    println(requestedEmails)
-
-    //var emails = myresult.map(x => x.profiles.email)
-    //myresult[0].user.profiles[0].email
-    //for (x <- Json.parse(myresult)) {println(x)}
-    println(Json.parse(myresult))
-    //val myjson = Json.parse(myresult)(0)
-    //println(myjson)
-    //println("looking at \\ profiles(0)")
-    //println((myjson \ "profiles")(0))
-    println("-----------------------------------")
-    //println("looking at \\ profiles(0) \\ email")
-    //println( ( (myjson \ "profiles")(0) \ "email" ) )
-    //var anEmail = ( (myjson \ "profiles")(0) \ "email" ).get
-
-    //val myjsonList = Json.parse(myresult)
-
-    //println("now trying with list and map")
-    //println(myjsonList)
-    
-    /*var emailsList = myjsonList.map ( x => 
-      ( (x \ "profiles")(0) \ "email" ).get
-    )
-    println(emailsList)
-*/
-
-    //((Json.parse(myresult) \ "suspendData") \ "d")(1)
-
-    Future(Ok(Json.parse(myresult)))
-  
+    var result = mailerService.getEmailAddress(listUuid)
+    println(result)
+    var itsmapped = result.map( option => option.get )
+    println(itsmapped)
+    Future(Ok(itsmapped.toString))
   }
 
-  /**
-* Returns a JSON object that represents a valid request body. It has to be used to request the user from Drops represented by the given UUIDs.
-*/
-def getEmailQuery(userIds: List[UUID]): JsValue = {
-  Json.obj("user" -> Json.obj(
-      "publicId" -> userIds.zipWithIndex.map(_.swap).foldLeft[JsObject](Json.obj())((json, i_id) =>
-        json ++ Json.obj(i_id._1.toString -> Json.toJson(i_id._2))
-      )
-    ))
-}
 
-  case class UserCrewRequest(userIds: List[UUID], dir: SortDir = Ascending) {
-//    println(this.toString)
-
-    /**
-      * Generates a JSON string equivalent to the request body.
-      *
-      * @author Johann Sell
-      * @return
-      */
-    override def toString: String = Json.obj(
-      "query" -> this.toQuery,
-      "values" -> this.toValues,
-      "sort" -> this.toSort
-    ).toString()
-
-    /**
-      * Generates the query string.
-      *
-      * @author Johann Sell
-      * @return
-      */
-    def toQuery : String = userIds.indices.map("user.publicId." + _ + ".=").mkString("_OR_")
-
-    /**
-      * Generates the JSON that can be used as values attribute of a drops REST query.
-      *
-      * @author Johann Sell
-      * @return
-      */
-    def toValues : JsValue = Json.obj("user" -> Json.obj(
-      "publicId" -> userIds.zipWithIndex.map(_.swap).foldLeft[JsObject](Json.obj())((json, i_id) =>
-        json ++ Json.obj(i_id._1.toString -> Json.toJson(i_id._2))
-      )
-    ))
-
-    /**
-      * Generates the JSON that can be used as sort attribute of a drops REST query.
-      * @return
-      */
-    def toSort : JsValue =
-      Json.obj("attributes" -> Json.toJson(List("SupporterCrew_name")), "dir" -> dir.name)
-  }
-
-  /**
-    * Companion object for request class.
-    *
-    * @author Johann Sell
-    */
-  object UserCrewRequest {
-    /**
-      * Generates JSON (Writes)
-      *
-      * @author Johann Sell
-      */
-    implicit val crewRequestWrites: Writes[UserCrewRequest] = (
-      (JsPath \ "query").write[String] and
-        (JsPath \ "values").write[JsValue] and
-        (JsPath \ "sort").write[JsValue]
-    )((request: UserCrewRequest) => (request.toQuery, request.toValues, request.toSort))
-  }
-
-    /**
-    * Represents a Drops REST response.
-    *
-    * @author Johann Sell
-    * @param id
-    * @param crew
-    */
-  case class UserResponse(id: UUID, crew: Option[UUID])
-
-  /**
-    * Companion object for a Drops REST repsonse.
-    *
-    * @author Johann Sell
-    */
-  object UserResponse {
-//    implicit val userResponseFormat = Json.format[UserResponse]
-    implicit val userResponseReads : Reads[UserResponse] = (
-      (JsPath \ "id").read[UUID] and
-        (JsPath \ "profiles" \\ "supporter" \ "crew" \ "id").readNullable[UUID]
-    )((userId, crewIds) => UserResponse(userId, crewIds))
-}
-  
-//end
-}
+} //end ApplicationController
